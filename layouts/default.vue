40bf817e458af8172a7723f329ee5d3a993a2f1e
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-04-29 20:39:07 -0300

    feat: created default layout :sparkles:

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<template>
+  <main
+    class="mx-auto w-full grid overflow-x-hidden overflow-y-auto bg-black min-h-screen"
+  >
+    <div
+      class="grid w-full lg:max-w-[1920px] lg:mx-auto bg-gray-200"
+    >
+      <slot />
+    </div>
+  </main>
+</template>
