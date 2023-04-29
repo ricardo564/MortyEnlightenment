ef818b4b42c0e255aa31ed61863c58c45ede0caf
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-04-29 20:42:25 -0300

    feat: created character interface :sparkles:

---
+++ models/Character.ts
@@ -0,0 +1,28 @@
+
+export interface ICharacter {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface IInfo {
+  count: number;
+  next: string;
+  pages: number;
+  prev: string;
+}
